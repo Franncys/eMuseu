940a70c2117c12c6d2e6f81d4cb72d3d35782b72
Franncys <franciscowebdeveloper@gmail.com>
2018-12-11 11:41:31 +0000

    [Project] - Project Creation

---
+++ eMuseu/eMuseu/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eMuseu.Startup))]
+namespace eMuseu
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
