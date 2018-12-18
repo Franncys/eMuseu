3b7d273a6a802ae95b3113e21e35015a05678d4b
KevinComel <krevin-cravalho@live.com.pt>
2018-12-18 13:12:56 +0000

    Login_Aprovado

--- eMuseu/eMuseu/Controllers/AccountController.cs
+++ eMuseu/eMuseu/Controllers/AccountController.cs
@@ -115,19 +115,23 @@ namespace eMuseu.Controllers
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
             var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
-            switch (result)
-            {
-                case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
-                case SignInStatus.LockedOut:
-                    return View("Lockout");
-                case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
-                case SignInStatus.Failure:
-                default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
-                    return View(model);
-            }
+            var list = context.Users.Where(u => u.UserName.Equals(model.UserName)).Select(u => u.aprovado);
+            if(list.Single())
+                switch (result)
+                {
+                    case SignInStatus.Success:
+                        return RedirectToLocal(returnUrl);
+                    case SignInStatus.LockedOut:
+                        return View("Lockout");
+                    case SignInStatus.RequiresVerification:
+                        return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                    case SignInStatus.Failure:
+                    default:
+                        ModelState.AddModelError("", "Invalid login attempt.");
+                        return View(model);
+                }
+            ModelState.AddModelError("", "Utilizador não aprovado!");
+            return View(model);
         }
 
         //
