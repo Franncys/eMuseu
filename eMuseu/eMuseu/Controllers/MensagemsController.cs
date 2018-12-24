6f4739fbcd75a7b97dbef357dad11c1e17c931f4
KevinComel <krevin-cravalho@live.com.pt>
2018-12-24 18:43:21 +0000

    Mensagem_modelo
    Criação do modelo das mensagens.

---
+++ eMuseu/eMuseu/Controllers/MensagemsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eMuseu.Models;
+
+namespace eMuseu.Controllers
+{
+    public class MensagemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Mensagems
+        public ActionResult Index()
+        {
+            return View(db.Mensagens.ToList());
+        }
+
+        // GET: Mensagems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mensagem mensagem = db.Mensagens.Find(id);
+            if (mensagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mensagem);
+        }
+
+        // GET: Mensagems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mensagems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MensagemID,OrigemID,DestinoID,EmailDest,Msg")] Mensagem mensagem)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                db.Mensagens.Add(mensagem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(mensagem);
+        }
+
+        // GET: Mensagems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mensagem mensagem = db.Mensagens.Find(id);
+            if (mensagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mensagem);
+        }
+
+        // POST: Mensagems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MensagemID,OrigemID,DestinoID,Msg")] Mensagem mensagem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(mensagem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(mensagem);
+        }
+
+        // GET: Mensagems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Mensagem mensagem = db.Mensagens.Find(id);
+            if (mensagem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(mensagem);
+        }
+
+        // POST: Mensagems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Mensagem mensagem = db.Mensagens.Find(id);
+            db.Mensagens.Remove(mensagem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
