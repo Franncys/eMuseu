40aa867734de9717ed11454f6ec34dd2f3640b51
KevinComel <krevin-cravalho@live.com.pt>
2018-12-31 13:53:40 +0000

    BootstrapV3_LayoutChanges

--- eMuseu/eMuseu/Models/Peca.cs
+++ eMuseu/eMuseu/Models/Peca.cs
@@ -12,10 +12,12 @@ namespace eMuseu.Models
     {
         [Key]
         public int PecaID { get; set; }
+        [Display(Name = "Nome da Peça")]
         public String nomePeca { get; set; }
         public int Periodo { get; set; }
         public String Zona { get; set; }
         public String Estado { get; set; }
+        [Display(Name = "Tipo de Peça")]
         public TipoPeca PecaTipo { get; set; }
         [ForeignKey("TratamentoID")]
         public IList<Tratamentos> TratamentoID { get; set; }
